5cfcb4ec597db1a5462790dd697993c56ad68b83
João Alves <joaoalves.cs@gmail.com>
2022-03-25 00:42:04 -0300

    Terminei o vídeo em 29m23

---
+++ notes/pages/index.tsx
@@ -0,0 +1,61 @@
+import type { NextPage } from 'next'
+import { useState } from 'react'
+
+interface FormData {
+  title: string
+  content: string
+  id: string
+}
+
+const Home: NextPage = () => {
+  const [form, setForm] = useState<FormData>({title: '', content: '', id: ''})
+  
+  async function create(data: FormData) {
+    try {
+      fetch('http://localhost:3000/api/create', {
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      }).then(() => setForm({ title: '', content: '', id: ''}))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleSubmit = async (data: FormData) => {
+    try {
+      create(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div>
+      <h1 className="text-center font-bold text-2xl mt-4">Notes</h1>
+      <form onSubmit={e => {
+        e.preventDefault()
+        handleSubmit(form)
+      }} className='w-auto min-w-[25%] max-w-min mx-auto space-y-6 flex flex-col items-stretch'>
+        <input type="text"
+          placeholder="Title"
+          value={form.title}
+          onChange={e => setForm({...form, title: e.target.value})}
+          className="border-2 rounded border-gray-600 p-1"
+        />
+        <textarea 
+          placeholder="Content"
+          value={form.content}
+          onChange={e => setForm({...form, content: e.target.value})}
+          className="border-2 rounded border-gray-600 p-1"
+        />
+        <button type="submit" className="bg-blue-500 text-white rounded p-1">Add +</button>
+      </form>
+    </div>
+  )
+}
+
+export default Home
+
